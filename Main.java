c43cc5e61518abe98433da6d074e6d2e63fd8479
kristinegev <christigevorgyan@yahoo.com>
2019-05-17 01:14:27 +0400

    commitment of uncommitted files

---
+++ Main.java
@@ -0,0 +1,14 @@
+package homework.managers;
+
+import homework.managers.MainController;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            MainController.getInstance().startworking();
+
+        }
+    }
+
